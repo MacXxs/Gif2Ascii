9851c3d6242ab6340492724a6f0038f32a534e8c
MacXxs <mikipmoline@gmail.com>
2025-06-13 20:34:12 +0200

    Added save flag to store the ascii representation in a .txt

--- src/gif2Asci.py
+++ src/gif2Asci.py
@@ -14,19 +14,22 @@ Usage:
 
 import argparse
 import sys
-import time
 from os import makedirs
+from pathlib import Path
 from PIL import Image, ImageSequence
-from time import sleep
+from time import sleep, time
 
 
 # Constants
-OUTPUT_DIR = "../out/"  # Directory to store output frames
+OUTPUT_DIR = Path(__file__).parent.parent / "out"  # Directory to store output frames
 TIME_PER_FRAME = 0.065  # Time to wait per frame in seconds (15 FPS)
-GRAYSCALE_GRADIENTS = ["@%#*+=-:. ",
-                       "@#%WM&8B*$oahkbdpqw. ",
-                       '''$@B%8&WM#*oahbdpqwmZO0QLCUYXzcvnxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.    ''']
+GRAYSCALE_GRADIENTS = ["@%#*+=-:. ", 
+                       "██▓▒░  ",
+                       "■■▣▩▨▢  "]
 GRAYSCALE_CHARS = GRAYSCALE_GRADIENTS[0]  # Default gradient
+
+# Flags
+SAVE_ENABLED = False  # Save frames to files instead of printing them
 
 
 def PrintFrame(asciiImg:list[str]):
@@ -92,9 +95,13 @@ def ProcessFrame(frame:Image.Image, imgWidth:int, imgHeight:int, tileWidth:float
     Process a single frame of the GIF and convert it to ASCII art.
     Args:
         frame (PIL.Image): The frame to process.
+        imgWidth (int): Width of the original image.
+        imgHeight (int): Height of the original image.
+        tileWidth (float): Width of each tile in the ASCII art.
+        tileHeight (float): Height of each tile in the ASCII art.
         width (int): Width of the ASCII art output.
         scale (float): Scale factor for height.
-        outPath (str): Path to save the processed frame.
+        inverse (bool): Whether to invert the ASCII gradient.
     """
     # ASCII image generation
     aImg = []
@@ -134,49 +141,84 @@ def ProcessFrame(frame:Image.Image, imgWidth:int, imgHeight:int, tileWidth:float
     return aImg
 
 
-def ProcessGif(videoPath:str, width:int, scale:float, inverse:bool = False) -> None:
+def SaveFrame(asciiImg:list[str], filename:str, frameNumber:int, writeMode:str = 'a') -> None:
+    """
+    Save the ASCII art representation of a frame to a file.
+    Args:
+        asciiImg (list[str]): List of strings representing the ASCII art.
+        filename (str): Name of the file to save the ASCII art.
+        frameNumber (int): Frame number to save.
+        writeMode (str): Mode to open the file.
+    """
+    # Write/append the ASCII art to a file
+    with open(f"{OUTPUT_DIR}/{filename}.txt", writeMode) as f:
+        f.write(f"Frame {frameNumber}\n")
+        for line in asciiImg:
+            f.write(line + '\n')
+
+
+def ProcessGif(videoPath:str, width:int, scale:float, frameToPrint:int = 0, inverse:bool = False) -> None:
     """
     Process a GIF file converting each frame to an ASCII art representation.
     Args:
         videoPath (str): Path to the GIF file.
         width (int): Width of the ASCII art output.
         scale (float): Scale factor for height.
+        inverse (bool): Whether to invert the ASCII characters.
     """
     # Create a directory to store the frames if it doesn't exist
     videoName = videoPath.split('/')[-1].split('.')[0]
-    outPath = f"{OUTPUT_DIR}/{videoName}"
-    makedirs(outPath, exist_ok=True)
 
-    # Process each frame of the GIF
     im = Image.open(videoPath)
     imgWidth, imgHeight, tileWidth, tileHeight, height = GetFrameData(im, width, scale)
 
-    prevTime = time.time()
-    for frame in ImageSequence.Iterator(im):
-        currentTime = time.time()
-        deltaTime = currentTime - prevTime
-        prevTime = currentTime
-
-        print(f"Frame number: {frame.tell()}")
-        
-        grayscale = frame.convert('L')
+    if frameToPrint != 0:
+        frame = frameToPrint - 1  # Convert to zero-based index
+        # If a specific frame is requested, seek to that frame
+        if frame < 0 or frame >= im.n_frames: # type: ignore
+            print(f"Frame {frameToPrint} is out of range. Total frames: {im.n_frames}") # type: ignore
+            exit(1)
+        im.seek(frame)
+        grayscale = im.convert('L')
         asciiImg = ProcessFrame(grayscale, imgWidth, imgHeight, tileWidth, tileHeight, width, height, inverse)
-        
-        PrintFrame(asciiImg)
-        if frame.tell() != im.n_frames - 1: # type: ignore
-            sys.stdout.write(f"\033[{len(asciiImg) + 1}A")  # Move cursor up to overwrite the previous frame
-        #grayscale.save(f"{outPath}/frame_{frame.tell()}.png")
+        PrintFrame(asciiImg) if not SAVE_ENABLED else SaveFrame(asciiImg, videoName, frame, 'w')
+    
+    else:
+        prevTime = time()
+        # Process each frame of the GIF
+        for frame in ImageSequence.Iterator(im):
 
-        if deltaTime < TIME_PER_FRAME:
-            sleep(TIME_PER_FRAME - deltaTime)  # Sleep to maintain frame rate
+            currentTime = time()
+            deltaTime = currentTime - prevTime
+            prevTime = currentTime
+
+            print(f"Frame number: {frame.tell()}")
+            
+            grayscale = frame.convert('L')
+            asciiImg = ProcessFrame(grayscale, imgWidth, imgHeight, tileWidth, tileHeight, width, height, inverse)
+            
+            if SAVE_ENABLED:
+                SaveFrame(asciiImg, videoName, frame.tell(), 'a')
+            else:
+                PrintFrame(asciiImg)
+                if frame.tell() != im.n_frames - 1: # type: ignore
+                    sys.stdout.write(f"\033[{len(asciiImg) + 1}A")  # Move cursor up to overwrite the previous frame
+
+            if deltaTime < TIME_PER_FRAME:
+                sleep(TIME_PER_FRAME - deltaTime)  # Sleep to maintain frame rate
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert a video file into ASCII art.')
+    parser = argparse.ArgumentParser(description='Convert a gif file into ASCII art.')
     parser.add_argument('video_file', type=str, help='Path to the video file to convert.')
     parser.add_argument('-w', '--width', type=int, default=80, help='Width of the ASCII art output (default: 80).')
-    parser.add_argument('-s', '--scale', type=float, default=0.42, help='Scale factor for height (default: 0.42).')
+    parser.add_argument('-s', '--scale', type=float, default=0.5, help='Scale factor for height (default: 0.42).')
     parser.add_argument('-i', '--inverse', action='store_true', help='Invert the ASCII characters (default: False).')
-    parser.add_argument('-g', '--gradient', type=int, default=0, choices=[0, 1, 2],)
+    parser.add_argument('-g', '--gradient', type=int, default=0, choices=[0, 1, 2])
+    parser.add_argument('-f', '--frame', type=int, default=0, help='Frame to print in a non-zero index. If not '
+                        'specified, all frames will be printed one after the other.')
+    parser.add_argument('-sv', '--save', action='store_true', 
+                        help='Save the ASCII art frames to a file instead of printing them.')
     
     args = parser.parse_args()
     
@@ -188,5 +230,6 @@ if __name__ == '__main__':
 
     # Set the grayscale gradient based on user input
     GRAYSCALE_CHARS = GRAYSCALE_GRADIENTS[args.gradient]
+    SAVE_ENABLED = args.save
 
-    ProcessGif(args.video_file, args.width, args.scale, args.inverse)
+    ProcessGif(args.video_file, args.width, args.scale, args.frame, args.inverse)
